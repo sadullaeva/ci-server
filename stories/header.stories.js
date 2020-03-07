3a5897b51d611913ec0e39e9d791dde678fab994
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-03-08 00:50:22 +0600

    Added header block

---
+++ stories/header.stories.js
@@ -0,0 +1,28 @@
+import "../src/style/main.css";
+
+export default {
+  title: 'Header',
+};
+
+export const Default = () => `
+  <h2>Primary header</h2>
+  <header class="header header_type_primary">
+    School CI server
+    <div class="header__extra">
+      <button type="button" class="button button_type_secondary button_size_s button_icon button_icon_play">
+        <span class="header__button-text">Run build</span>
+      </button>
+      <button type="button" class="button button_type_secondary button_size_xs button_icon button_icon_settings"></button>
+    </div>
+  </header>
+
+  <h2>Secondary header</h2>
+  <header class="header header_type_secondary">
+    School CI server
+    <div class="header__extra">
+      <button type="button" class="button button_type_secondary button_size_s button_icon button_icon_settings">
+        <span class="header__button-text">Settings</span>
+      </button>
+    </div>
+  </header>
+`;
