1e64fec584a36494ac461d5abe5232c589914b2e
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-03-08 14:44:10 +0600

    Added link block

---
+++ stories/link.stories.js
@@ -0,0 +1,15 @@
+import "../src/style/main.css";
+
+export default {
+  title: 'Link',
+};
+
+export const Default = () => `
+  <h2>Link</h2>
+  <a href="#" class="link link_type_primary">
+    Primary
+  </a>
+  <a href="#" class="link link_type_secondary">
+    Secondary
+  </a>
+`;
