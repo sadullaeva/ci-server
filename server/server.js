54b632624ab7c14f79426c612ffff4367e451fdb
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-03-14 00:29:16 +0600

    Added swagger.json and applied swagger-ui-express

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const api = require('./api');
 
 const app = express();
 
+app.use(express.json());
+
+const swaggerConfig = require('./swagger.json');
+
 app.use(express.static(path.resolve(__dirname, '../client')));
 
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 app.use('/api', api);
 app.use('*', (req, res) => {
   res.sendStatus(404);
