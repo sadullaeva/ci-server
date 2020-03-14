a71ff0a853b59201b4c37e6dd123d5ef011442ab
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-03-14 19:25:44 +0600

    Added storage methods for start, finish and cancel build

--- server/services/storageMethods.js
+++ server/services/storageMethods.js
@@ -8,6 +8,12 @@ exports.getBuild = search => axios.get(`/build/details${search}`);
 
 exports.getBuildLog = search => axios.get(`/build/log${search}`);
 
-exports.getBuilds = (search) => axios.get(`/build/list${search}`);
+exports.getBuilds = search => axios.get(`/build/list${search}`);
 
 exports.postBuild = body => axios.post('/build/request', body);
+
+exports.startBuild = body => axios.post('/build/start', body);
+
+exports.finishBuild = body => axios.post('/build/finish', body);
+
+exports.cancelBuild = body => axios.post('/build/cancel', body);
