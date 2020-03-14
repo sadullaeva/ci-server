8b6070746c5ba1c353b39ea798434475f1efa229
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-03-14 16:45:05 +0600

    Added cloning repo when post settings

--- server/settings.js
+++ server/settings.js
@@ -1,4 +1,5 @@
 const axios = require('./axios');
+const cloneRepo = require('./services/cloneRepo');
 
 exports.getSettings = (req, res, next) => {
   axios
@@ -12,6 +13,9 @@ exports.getSettings = (req, res, next) => {
 };
 
 exports.postSettings = (req, res, next) => {
+  const { repoName, mainBranch } = req.body;
+  cloneRepo(repoName, mainBranch);
+
   axios
     .post(`/conf`, req.body)
     .then(response => {
