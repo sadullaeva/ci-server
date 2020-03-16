cf0938a12634b0d5d9b407c8937cc41286c7bdcb
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-03-16 19:26:47 +0600

    Added deleting of oldest logs from cache when max cache size is reached

--- server/services/cacheLog.js
+++ server/services/cacheLog.js
@@ -1,33 +1,39 @@
-const cache = {};
+const cacheMap = new Map();
+// const MAX_CACHE_SIZE = 2; // for testing
 const MAX_CACHE_SIZE = 10;
 const STORAGE_TIME = 600000; // 10 min
 
-const setClearTimer = (buildId) => {
+const setClearTimer = buildId => {
   const timerId = setTimeout(() => {
-    delete cache[buildId];
+    cacheMap.delete(buildId);
     clearTimeout(timerId);
   }, STORAGE_TIME);
   return timerId;
 };
 
 const cacheLog = (buildId, log) => {
-  const cachedLog = cache[buildId];
+  const cachedLog = cacheMap.get(buildId);
 
   if (cachedLog) {
     clearTimeout(cachedLog.timerId);
     cachedLog.timerId = setClearTimer(buildId);
     cachedLog.dateTime = new Date();
   } else {
-    cache[buildId] = {
+    while (cacheMap.size >= MAX_CACHE_SIZE) {
+      const cacheMapIterator = cacheMap.keys();
+      const keyToDelete = cacheMapIterator.next().value;
+      cacheMap.delete(keyToDelete);
+    }
+    cacheMap.set(buildId, {
       log,
       dateTime: new Date(),
-      timerId: setClearTimer(buildId)
-    }
+      timerId: setClearTimer(buildId),
+    });
   }
 };
 
-const getFromCache = (buildId) => {
-  const cachedLog = cache[buildId];
+const getFromCache = buildId => {
+  const cachedLog = cacheMap.get(buildId);
 
   if (cachedLog) {
     clearTimeout(cachedLog.timerId);
