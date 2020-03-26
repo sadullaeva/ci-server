a3c982f23df0ec3dd7ce46633448a8f64bdd92bb
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-03-26 23:04:08 +0600

    Set up bem naming style

--- client/src/base.blocks/contentBox/contentBox.jsx
+++ client/src/base.blocks/contentBox/contentBox.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import { cn } from '@bem-react/classname';
+import { cn } from 'utils/bem';
 
 import './contentBox.css';
 
