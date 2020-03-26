9704314abfdecfb2bc18014f5d401b76c431920d
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-03-26 21:55:19 +0600

    Added IconText component

---
+++ client/src/base.blocks/iconText/iconText.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { cn } from '@bem-react/classname';
+
+import './iconText.css';
+
+const IconText = props => {
+  const { className = '', children, icon, type = 'primary', ...other } = props;
+  const iconText = cn('icon-text');
+  const classes = clsx(iconText, icon && iconText({ icon }), iconText({ type }));
+  return (
+    <div className={classes} {...other}>
+      {children}
+    </div>
+  );
+};
+
+IconText.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+  type: PropTypes.oneOf(['primary', 'secondary']),
+  icon: PropTypes.oneOf(['calendar', 'stopwatch', 'commit', 'user']),
+};
+
+export default IconText;
