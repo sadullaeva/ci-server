ff1cd1cc050c872fff110852f4cdc88414d46651
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-03-27 20:28:04 +0600

    Added Footer block

---
+++ client/src/stories/footer.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Footer from 'src/content.blocks/footer/footer';
+
+export default {
+  title: 'Footer',
+  component: Footer,
+};
+
+export const Default = () => <Footer />;
