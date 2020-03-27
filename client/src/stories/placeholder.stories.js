f9b687367b9c29523b944590139cb02c9603b464
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-03-27 21:07:07 +0600

    Added Placeholder component

---
+++ client/src/stories/placeholder.stories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Placeholder from 'src/content.blocks/placeholder/placeholder';
+
+export default {
+  title: 'Placeholder',
+  component: Placeholder,
+};
+
+export const Default = () => (
+  <>
+    <h2>Start page placeholder</h2>
+    <Placeholder
+      action={{
+        content: 'Open settings',
+        onClick: () => action('clicked'),
+      }}
+    >
+      Configure repository connection and synchronization settings
+    </Placeholder>
+  </>
+);
