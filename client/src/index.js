8ef003d1d27466048b565652e889a7270d424236
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-03-29 00:38:31 +0600

    Added RouterWrapper

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import * as serviceWorker from './serviceWorker';
 import store from './store/store';
 
 import PrivateRoute from './template.blocks/privateRoute/privateRoute';
+import RouterWrapper from './template.blocks/routerWrapper/routerWrapper';
 
 import SettingsPage from './pages/settingsPage/settingsPage';
 import BuildPage from './pages/buildPage/buildPage';
@@ -16,32 +17,34 @@ import MainPage from './pages/mainPage/mainPage';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route exact path={'/'}>
-            <MainPage />
-          </Route>
-          <Route path={'/settings'}>
-            <SettingsPage />
-          </Route>
-          <PrivateRoute path={'/build/:number'}>
-            <BuildPage
-              heading={'philip1967/my-awesome-repo'}
-              build={{
-                status: 'success',
-                buildNumber: 1368,
-                message: 'add documentation for postgres scaler',
-                branch: 'master',
-                commit: '9c9f0b9',
-                author: 'Philip Kirkorov',
-                date: '21 янв, 03:06',
-                duration: '1 ч 20 мин',
-              }}
-            />
-          </PrivateRoute>
-          <Route>{'404'}</Route>
-        </Switch>
-      </Router>
+      <RouterWrapper>
+        <Router>
+          <Switch>
+            <Route exact path={'/'}>
+              <MainPage />
+            </Route>
+            <Route path={'/settings'}>
+              <SettingsPage />
+            </Route>
+            <PrivateRoute path={'/build/:number'}>
+              <BuildPage
+                heading={'philip1967/my-awesome-repo'}
+                build={{
+                  status: 'success',
+                  buildNumber: 1368,
+                  message: 'add documentation for postgres scaler',
+                  branch: 'master',
+                  commit: '9c9f0b9',
+                  author: 'Philip Kirkorov',
+                  date: '21 янв, 03:06',
+                  duration: '1 ч 20 мин',
+                }}
+              />
+            </PrivateRoute>
+            <Route>{'404'}</Route>
+          </Switch>
+        </Router>
+      </RouterWrapper>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- client/src/pages/mainPage/mainPage.jsx
+++ client/src/pages/mainPage/mainPage.jsx
@@ -1,20 +1,11 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 import HistoryPage from 'pages/historyPage/historyPage';
 import StartPage from 'pages/startPage/startPage';
 
-import { getSettings } from 'store/settings/getSettings';
-
 const MainPage = () => {
-  const dispatch = useDispatch();
   const settings = useSelector(state => state.settings.settings);
-
-  useEffect(() => {
-    if (!settings) {
-      dispatch(getSettings());
-    }
-  }, [settings, dispatch]);
 
   return !!settings ? <HistoryPage /> : <StartPage />;
 };
