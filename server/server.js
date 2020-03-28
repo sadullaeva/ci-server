2f3cd4f620eb0b226f13bc87de1ffa6b52af4db3
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-03-28 20:57:06 +0600

    Added allowing cors from localhost:3000

--- server/server.js
+++ server/server.js
@@ -3,17 +3,19 @@ require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 const api = require('./routes/api');
 
 const app = express();
 
 app.use(express.json());
-
-const swaggerConfig = require('./swagger.json');
-
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+const swaggerConfig = require('./swagger.json');
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+
 app.use('/api', api);
 app.use('*', (req, res) => {
   res.sendStatus(404);
