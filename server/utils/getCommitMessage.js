c30cee1679d3adc25035faa1f0d0ffbd95ffb254
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-04-13 01:02:56 +0600

    Refactored getCommitMessage function

--- server/utils/getCommitMessage.js
+++ server/utils/getCommitMessage.js
@@ -1,13 +1,10 @@
 const path = require('path');
-const { exec } = require('child_process');
-const promisifyChildProcess = require('./promisifyChildProcess');
+const child_process = require('./childProcess');
 
-module.exports = commitHash => {
-  let commitMessage;
+module.exports = async commitHash => {
   const repoPath = path.resolve(__dirname, '../ci-repo');
-  const child = exec(`git -C ${repoPath} log -1 --format="%s" ${commitHash}`);
-  child.stdout.on('data', data => {
-    commitMessage = data;
-  });
-  return promisifyChildProcess(child).then(() => commitMessage.trim());
+  const { stdout: commitMessage = '' } = await child_process.exec(
+    `git -C ${repoPath} log -1 --format="%s" ${commitHash}`
+  );
+  return commitMessage.trim();
 };
