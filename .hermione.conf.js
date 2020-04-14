4536e00b7f5e0171b917f87e21e7b7c5f9ac87d4
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-04-15 00:19:40 +0600

    Added html-reporter, updated hermione config

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -1,14 +1,24 @@
 module.exports = {
+  baseUrl: 'http://localhost:3000',
+  gridUrl: 'http://localhost:4444/wd/hub',
+
   sets: {
     common: {
       files: 'test/**/*.hermione.js',
     },
   },
+
   browsers: {
     chrome: {
       desiredCapabilities: {
         browserName: 'chrome',
       },
+    },
+  },
+
+  plugins: {
+    'html-reporter/hermione': {
+      path: 'hermione-html-report',
     },
   },
 };
