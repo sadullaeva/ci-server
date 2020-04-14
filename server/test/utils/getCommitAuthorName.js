4fd37fbf5a224f6a8bd46d6e388dabf774fcca45
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-04-14 19:41:58 +0600

    Added tests for getCommitAuthorName function

---
+++ server/test/utils/getCommitAuthorName.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const child_process = require('../../utils/childProcess');
+
+const getCommitAuthorName = require('../../utils/getCommitAuthorName');
+
+const authorName = 'Commit message';
+const sandbox = sinon.createSandbox();
+
+beforeEach(() => {
+  sandbox.stub(child_process, 'exec').resolves({ stdout: authorName });
+});
+
+afterEach(() => {
+  sandbox.restore();
+});
+
+describe('getCommitAuthorName', () => {
+  it('gets commit author name from git log', async () => {
+    const commitHash = '1q2w3e4';
+
+    const result = await getCommitAuthorName(commitHash);
+
+    expect(result).to.equal(authorName);
+  });
+});
