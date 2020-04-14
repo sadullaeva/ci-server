3ced62ba452ba96ca60b611914bbc4922ea5ff84
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-04-14 21:15:03 +0600

    Refactored cloneRepo function

--- server/services/cloneRepo.js
+++ server/services/cloneRepo.js
@@ -1,27 +1,18 @@
 const path = require('path');
-const { spawn, exec } = require('child_process');
+const child_process = require('../utils/childProcess');
 
-module.exports = (repoName, mainBranch) =>
-  new Promise((resolve, reject) => {
-    const cwd = path.resolve(__dirname, '..');
-    const folderName = 'ci-repo';
+module.exports = async (repoName, mainBranch) => {
+  const cwd = path.resolve(__dirname, '..');
+  const folderName = 'ci-repo';
 
-    exec(`rm -rf ${cwd}/${folderName}`, () => {
-      const gitClone = spawn(
-        'git',
-        ['clone', '-b', mainBranch, `git@github.com:${repoName}.git`, folderName],
-        { cwd }
-      );
-
-      gitClone.stdout.pipe(process.stdout);
-      gitClone.stderr.pipe(process.stdout);
-      gitClone.on('exit', code => {
-        console.log(`Cloning child process exited with code ${code}`);
-        if (code === 0) {
-          resolve();
-        } else {
-          reject();
-        }
-      });
-    });
-  });
+  try {
+    await child_process.exec(`rm -rf ${cwd}/${folderName}`);
+    await child_process.spawn([
+      'git',
+      ['clone', '-b', mainBranch, `git@github.com:${repoName}.git`, folderName],
+      { cwd },
+    ]);
+  } catch (e) {
+    console.log('Could not clone the repo', e);
+  }
+};
