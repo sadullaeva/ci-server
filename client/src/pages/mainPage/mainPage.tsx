4a270aff3d698acb2a89b0eb7c2b558c4d3a2a8b
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-04-24 19:37:42 +0600

    Added TS for Main page

--- client/src/pages/mainPage/mainPage.tsx
+++ client/src/pages/mainPage/mainPage.tsx
@@ -4,8 +4,11 @@ import { useSelector } from 'react-redux';
 import HistoryPage from 'pages/historyPage/historyPage';
 import StartPage from 'pages/startPage/startPage';
 
+import { State } from 'store/store';
+import Settings from 'typings/settings';
+
 const MainPage = () => {
-  const settings = useSelector(state => state.settings.settings);
+  const settings = useSelector((state: State): Settings => state.settings.settings);
 
   return !!settings ? <HistoryPage /> : <StartPage />;
 };
